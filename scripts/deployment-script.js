a89159e00ce405fc8e25cf003cabfa27e10e9786
carter_laptop <carter_laptop@pop-os.localdomain>
2022-06-08 20:28:41 -0500

    erc721

--- scripts/deployment-script.js
+++ scripts/deployment-script.js
@@ -14,12 +14,12 @@ async function main() {
   // await hre.run('compile');
 
   // We get the contract to deploy
-  const Greeter = await hre.ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  const MembershipNFT = await hre.ethers.getContractFactory("MembershipNFT");
+  const membershipNFT = await MembershipNFT.deploy();
 
-  await greeter.deployed();
+  await membershipNFT.deployed();
 
-  console.log("Greeter deployed to:", greeter.address);
+  console.log("MembershipNFT deployed to:", membershipNFT.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
