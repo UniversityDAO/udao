e9ab823c4f2733f0e01e113d90cfe4cd7017e3cd
CptAstro <austindd@outlook.com>
2022-06-10 18:49:28 -0500

    Added metamask wallet connect

---
+++ frontend/src/components/Buttons/MetamaskConnect.js
@@ -0,0 +1,89 @@
+import MetaMaskOnboarding from '@metamask/onboarding';
+import React, { useRef, useEffect } from 'react';
+
+function MetamaskConnect ()
+{
+
+const ref1=useRef(null);
+const ref2=useRef(null);
+const ref3=useRef(null);
+
+const onboarding = new MetaMaskOnboarding();
+/*const btn = document.querySelector('.onboard');
+const statusText = document.querySelector('.h1-connect');
+const statusDesc = document.querySelector('.connect-desc');*/
+
+useEffect(() => {
+    const btn = ref1.current;
+    const statusText=ref2.current;
+    const statusDesc=ref3.current;
+
+
+    const { ethereum } = window;
+
+
+    const isMetaMaskInstalled = () => {
+        return Boolean(ethereum && ethereum.isMetaMask);
+    }
+
+    let connected = (accounts) => {
+        statusText.innerHTML = 'Connected!'
+        statusDesc.classList.add('account');
+        statusDesc.innerHTML = accounts[0]
+        btn.style.display = 'none';
+        statusDesc.classList.add('account');
+    }
+
+    async function connectWallet() {
+        return await ethereum.request({ method: 'eth_accounts' });
+    }
+
+    const onClickInstallMetaMask = () => {
+        onboarding.startOnboarding();
+    }
+
+    btn.addEventListener('click', async () => {
+        btn.style.backgroundColor = '#cccccc';
+
+        try {
+            const accounts = await ethereum.request({method: 'eth_requestAccounts'})
+            connected(accounts)
+        } catch (error) {
+            console.error(error);
+        }
+    })
+
+    const MetaMaskClientCheck = () => {
+        if (!isMetaMaskInstalled()) {
+            statusText.innerText = 'You need to Install a Wallet';
+            statusDesc.innerText = 'We recommend the MetaMask wallet.';
+            btn.innerText = 'Install MetaMask'
+            btn.onclick = onClickInstallMetaMask;
+        } else {
+    
+            connectWallet().then((accounts) => {
+                if (accounts && accounts[0] > 0) {
+                    connected(accounts)
+                } else {
+                    statusText.innerHTML = 'Connect your wallet'
+                    statusDesc.innerHTML = `To begin, please connect your MetaMask wallet.`
+                    btn.innerText = 'Connect MetaMask'
+                }
+            })
+        }
+    }
+
+    MetaMaskClientCheck();
+
+});
+
+return (
+    <>
+        <button ref={ref1} /*class="btn btn-primary onboard"*/ style={{textAlign:'right', marginTop: "20px"}}>Connect Wallet</button>
+        <h1 ref={ref2} /*class="h1-connect"*/> </h1>
+        <p ref={ref3} /*class="connect-desc" */></p>
+    </>
+    )
+}
+
+export default MetamaskConnect;
