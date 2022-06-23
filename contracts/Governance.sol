6d198dccb13999ca545de9a45aadf0e793c53ff4
cartercameron1 <95325774+cartercameron1@users.noreply.github.com>
2022-06-22 23:00:21 -0500

    draft governance contract and deployment script needs additional testing (#18)
    Co-authored-by: carter_laptop <carter_laptop@pop-os.localdomain>

---
+++ contracts/Governance.sol
@@ -0,0 +1,49 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/governance/Governor.sol";
+import "@openzeppelin/contracts/governance/extensions/GovernorSettings.sol";
+import "@openzeppelin/contracts/governance/extensions/GovernorCountingSimple.sol";
+import "@openzeppelin/contracts/governance/extensions/GovernorVotes.sol";
+
+contract Governance is Governor, GovernorSettings, GovernorCountingSimple, GovernorVotes {
+    constructor(IVotes _token)
+        Governor("MyGovernor")
+        GovernorSettings(1 /* 1 block */, 45818 /* 1 week */, 1)
+        GovernorVotes(_token)
+    {}
+
+    function quorum(uint256 blockNumber) public pure override returns (uint256) {
+        return 3;
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function votingDelay()
+        public
+        view
+        override(IGovernor, GovernorSettings)
+        returns (uint256)
+    {
+        return super.votingDelay();
+    }
+
+    function votingPeriod()
+        public
+        view
+        override(IGovernor, GovernorSettings)
+        returns (uint256)
+    {
+        return super.votingPeriod();
+    }
+
+    function proposalThreshold()
+        public
+        view
+        override(Governor, GovernorSettings)
+        returns (uint256)
+    {
+        return super.proposalThreshold();
+    }
+}
+
