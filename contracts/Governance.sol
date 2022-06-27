d9ae148108f841397f52fb6d289a7ce2ed313a18
Victor <56770457+victor-wei126@users.noreply.github.com>
2022-06-26 21:08:43 -0500

    Add draft README.md and comments to scripts (#24)
    * add readme draft
    
    * add some comments to governance contract for clarity
    
    * add comments to scripts

--- contracts/Governance.sol
+++ contracts/Governance.sol
@@ -8,6 +8,9 @@ import "@openzeppelin/contracts/governance/extensions/GovernorVotes.sol";
 contract Governance is Governor, GovernorCountingSimple, GovernorVotes {
     constructor(IVotes _token) Governor("MyGovernor") GovernorVotes(_token) {}
 
+    // NOTE: voting delay won't really matter since everyone's voting power is effectively fixed--
+    // each member can have one and only one MembershipNFT. They cannot purchase or obtain more.
+    // the only thing a delay will serve us is additional time before voting begins
     function votingDelay() public pure override returns (uint256) {
         return 1; // 1 block
     }
@@ -17,9 +20,16 @@ contract Governance is Governor, GovernorCountingSimple, GovernorVotes {
     }
 
     function quorum(uint256 blockNumber) public pure override returns (uint256) {
-        return 1;
+        return 1; // for now, set to 1 for easier testing
     }
 
+    // The number of votes required for a voter to become a proposer, ie, make a proposal
+    // for the DAO.
+    // I think what this means is how many "tokens" one must have in order to make a proposal.
+    // "votes" refers to "tokens" here because the entire voting system relies on whether
+    // someone has tokens (ERC20, or ERC721). "votes" does not refer to the traditional sense
+    // of "votes", ie, an indication of some choice.
+    // So we set the threshold to 1, since a user must have 1 MembershipNFT in order to vote.
     function proposalThreshold() public pure override returns (uint256) {
         return 1;
     }
