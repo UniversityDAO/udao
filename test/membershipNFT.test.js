c12648d2451b9a4571694e83d1c23d84dc237b5e
Victor Wei <vw1262@gmail.com>
2022-06-29 21:30:34 -0500

    add some NFT minting tests

--- test/membershipNFT.test.js
+++ test/membershipNFT.test.js
@@ -1,24 +1,37 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
-describe("MembershipNFT", () => {
-  it("is deployed correctly", async () => {
-    const MembershipNFT = await ethers.getContractFactory("MembershipNFT");
-    const membershipNFT = await MembershipNFT.deploy("UDAOMember", "UDM");
-    await membershipNFT.deployed();
+describe("MembershipNFT", async () => {
+  let accounts;
+  let membershipNFT;
 
-    expect(await membershipNFT.name()).to.equal("UDAOMember");
-    expect(await membershipNFT.symbol()).to.equal("UDM");
-    // expect(await membershipNFT.owner()).to.equal(account[1]);
+  // deploy contract and get HRE accounts
+  before(async () => {
+    // get the array of hard hat accounts
+    accounts = await (await ethers.getSigners()).map((signer) => signer.address);
+
+    const MembershipNFT = await ethers.getContractFactory("MembershipNFT");
+    membershipNFT = await MembershipNFT.deploy("UDAOMember", "UDM");
+    await membershipNFT.deployed();
   });
 
-  //TODO: check that _mint can't be called externally
-  it("only allows owner to mint", async () => {
-    
+  it("is deployed correctly", async () => {
+    expect(await membershipNFT.name()).to.equal("UDAOMember");
+    expect(await membershipNFT.symbol()).to.equal("UDM");
+    expect(await membershipNFT.owner()).to.equal(accounts[0]);
   });
 
   // check that counter is incremented, NFT received correctly, updates voting power
   it("mints NFTs correctly", async () => {
+    let receiver = accounts[1];
 
+    await membershipNFT.safeMint(receiver);
+    let balance = await membershipNFT.balanceOf(receiver);
+    let owner = await membershipNFT.ownerOf(0);
+
+    //TODO: check voting power (before & after token transfer)
+
+    expect(balance).to.equal(1);
+    expect(owner).to.equal(receiver);
   });
 })
