6e575d101ea40abee94dc1867f4c7889c05b7ae4
CptAstro <austindd@outlook.com>
2022-07-15 20:27:35 -0500

    Added documentation to EthersAPI.js

--- frontend/src/data/EthersApi.js
+++ frontend/src/data/EthersApi.js
@@ -2,10 +2,22 @@ import { ethers } from 'ethers';
 import { govAddress, tokenAddress, nftAddress } from './config';
 import { govAbi, tokenAbi, nftAbi } from './config';
 
+/**
+ * This function will be used to take our proposal data from the application
+ * and pass it through the Keccak256 hash to be used in the Solidity propsose() function
+ * @param {Object} jsonObject - A proposal/grant JSON object just containing fields from the application form
+ * @returns {Keccak256 Hash} - ethers.utils.id() generates a Keccak256 hash for whatever is passed in as an argument
+ */
 export function generateHash(jsonObject) {
     return ethers.utils.id(jsonObject);
 }
 
+/**
+ * This function takes in a Proposal ID and then accesses the governance 
+ * contract to see what the voting data is for this proposal
+ * @param {uint256} proposalId - The Proposal ID for the proposal we want to retrieve data for
+ * @returns {Object} - Return an object of format {forVotes, againstVotes, obstainVotes}
+ */
 export async function getVoteData(proposalId) {
     let address = govAddress();
     let abi = govAbi();
@@ -16,6 +28,12 @@ export async function getVoteData(proposalId) {
     return voteData;
 }
 
+/**
+ * This function takes in a Proposal ID and then accesses the governance
+ * contract to see if this proposal is active or not 
+ * @param {uint256} proposalId - The Proposal ID for the proposal we want to retrieve data for
+ * @returns {enum} - Return an enum where active == 1. See enum IGovernor.ProposalState on Open Zeppelin
+ */
 export async function getActiveStatus(proposalId) {
     let address = govAddress();
     let abi = govAbi();
