cdac4f7b289ac9fd59d2a0d3042874df7a53843b
CptAstro <austindd@outlook.com>
2022-08-08 14:25:54 -0500

    Merged PR 61

--- frontend/src/pages/loading.js
+++ frontend/src/pages/loading.js
@@ -6,19 +6,19 @@ import { useSelector } from "react-redux";
 
 export default function LoadingPage() {
     let loading = useSelector(state => state.isLoading);
-        if (loading) {
-            return (
-                <div className="loading-page">
-                    <div className="loading-content">
-                        <Loading />
-                        <span>Loading Data...</span>
-                    </div>
+    if (loading) {
+        return (
+            <div className="loading-page">
+                <div className="loading-content">
+                    <Loading />
+                    <span>Loading Data...</span>
                 </div>
-            )
-        }
-        else {
-            return (
-                <Navigate to="/Dashboard" replace={true}/>
-            )
-        }
+            </div>
+        )
+    }
+    else {
+        return (
+            <Navigate to="/Dashboard" replace={true}/>
+        )
+    }
 }
