1ddd326c70e52aa7d9f3ffad8841168078d66a49
Jagger Harris <78388435+jagger-harris@users.noreply.github.com>
2022-08-26 16:41:05 -0500

    Temporary comment out (#72)

--- src/components/ConnectWallet.jsx
+++ src/components/ConnectWallet.jsx
@@ -6,6 +6,7 @@ function ConnectWallet() {
       {/*This is the original stylization, not for testing*/}
       {/*<div className="p-5 flex flex-col justify-center items-center fixed inset-0 w-screen h-screen bg-black/50">*/}
       {/*Testing style*/}
+      {/*
         <div className="inset-15 fixed w-48 h-48 rounded-xl bg-gray">
           Metamask not installed, code here
         </div>
@@ -14,7 +15,7 @@ function ConnectWallet() {
         </div>
         <div className="inset-15 top-[30rem] fixed w-48 h-48 rounded-xl bg-gray">
           Returning user, code here
-        </div>
+  </div>*/}
     </>
   )
 }
