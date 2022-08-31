1ba2dd9ac1000ffd389731678e25ed1b31d66331
CptAstro <70530319+CptAstro@users.noreply.github.com>
2022-08-31 16:41:15 -0500

    Proposal Redirection

--- src/pages/SubmittingProposal.jsx
+++ src/pages/SubmittingProposal.jsx
@@ -1,59 +1,54 @@
 import Loading from "../components/LoadingSymbol";
 import { Link } from "react-router-dom";
-import { useSelector } from "react-redux/es/exports";
-
-
-import * as web3Storage from '../api/web3StorageAPI'
-import { propose } from "../api/EthersApi"
+import { useSelector } from "react-redux";
 import { GOV_ADDRESS_MUMBAI, GOV_ABI } from "../data/config";
 
-export default async function SubmittingProposal() {
+import { upload } from "../api/web3StorageAPI";
+import { propose } from "../api/EthersApi";
 
-    let metadata = useSelector(state => state.currentProposalMetadata);
-    let slicedString = useSelector(state => state.currentProposalTitle);
+export default function SubmittingProposal() {
+    let metadata = useSelector(state => state.CurrentProposalMetadata);
+    let title = useSelector(state => state.CurrentProposalTitle);
+    let provider = useSelector(state => state.metamaskProvider);
 
     let jsonFile = makeFileObjects(metadata);
 
-
-    const provider = useSelector(state => state.metamaskProvider);
-    // TODO: find way to get cid w/o uploading. then wait for blockchain txn to confirm
-    // first before then finalling uploading to IPFS (or else we might have files in limbo)
-    let ipfs_cid = await web3Storage.upload(jsonFile, `Proposal-${slicedString}`, true);
-
-    // create a proposal on blockchain
-    let proposal_id = await propose([GOV_ADDRESS_MUMBAI, GOV_ABI, provider], ipfs_cid);
-    // can now do something with proposal_id, like fetch and render or something
+    let ipfs_cid = upload(jsonFile, `Proposal-${title}`, true);
+    let proposal_id = propose([GOV_ADDRESS_MUMBAI, GOV_ABI, provider], ipfs_cid);
 
     function makeFileObjects (obj) {
-      const blob = 
-          new Blob([JSON.stringify(obj)], { type: 'application/json'})
-      const files = [
-          new File([blob], 'Proposal.json')
-      ]
-      return files;
-  }
+        const blob = 
+            new Blob([JSON.stringify(obj)], { type: 'application/json'})
+        const files = [
+            new File([blob], 'Proposal.json')
+        ]
+        return files;
+    }
 
-    if (!(ipfs_cid && proposal_id)) {
-        return (
-        <div className="mb-5 mr-5 p-5 w-full h-full flex flex-col rounded-lg bg-black">
-            <p className="text-3xl">Proposal Currently Submitting...</p>
-            <Loading />
-        </div>
-        )
+    function DisplayLoading() {
+        if (!(ipfs_cid && proposal_id)) {
+            return (
+            <div className="mb-5 mr-5 p-5 w-full h-full flex flex-col rounded-lg bg-black">
+                <p className="text-3xl">Proposal Currently Submitting...</p>
+                <Loading />
+            </div>
+            )
+        }
+        else {
+            return (
+                <div className="mb-5 mr-5 p-5 w-full h-full flex flex-col rounded-lg bg-black">
+                <p className="text-3xl">Proposal is now on the blockchain</p>
+                <Link to="/proposals">
+                    <button>Click here to return to proposals</button>
+                </Link>
+              </div>
+            )
+        }
     }
-    else {
-        return (
-          <div className="mb-5 mr-5 p-5 w-full h-full flex flex-col rounded-lg bg-black">
-            <p className="text-3xl">Proposal is now on the blockchain</p>
-            <Link to="/proposals">
-                <button>Click here to return to proposals</button>
-            </Link>
-          </div>
-        )
-    }
+
     return (
-      <div>
-        Testing testing
-      </div>
+        <div>
+            <DisplayLoading/>
+        </div>
     )
 }
