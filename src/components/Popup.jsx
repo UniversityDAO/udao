e37e7d5775d0ccb649f3f4b61b16a9985ec7f9c9
jagger.lee.harris@gmail.com <jagger.lee.harris@gmail.com>
2022-09-27 14:11:58 -0500

    Reversed proposals from newest to oldest

--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -9,7 +9,7 @@ function Popup({isShown, onCloseClick, message, button}) {
   
   return (
     !isShown ? null :
-      <div className={"p-5 flex flex-col justify-center items-center fixed top-0 left-0 w-screen h-screen bg-black/80"}>
+      <div className="z-50 p-5 flex flex-col justify-center items-center fixed top-0 left-0 w-screen h-screen bg-black/80">
         <div className="p-5 flex flex-col justify-center rounded-lg bg-gray">
         <button onClick={handleCloseClick} className="pt-3 pb-6 flex justify-center items-center w-6 h-6 rounded-lg text-lg cursor-pointer hover:text-white"><Close/></button>
         <div className="flex flex-col justify-center items-center">
