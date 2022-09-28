28caf18a1d10bd37f33bdb731d53dc5be69b3ba7
jagger.lee.harris@gmail.com <jagger.lee.harris@gmail.com>
2022-09-27 20:10:48 -0500

    Changed 5 to 6 and added functionality to see more button

--- src/components/Tag.jsx
+++ src/components/Tag.jsx
@@ -14,11 +14,10 @@ function Tag({proposal}) {
 
   return (
     <div className={"transition-all duration-200 mr-2.5 p-2.5 pt-1 pb-1 rounded-lg flex justify-center items-center " +
-                    (proposal.state == 4 || proposal.state == 7 ?
-                      "bg-green"
-                        : (proposal.state == 2 || proposal.state == 3 || proposal.state == 5 ?
-                          "bg-red" : "bg-purple"))
-                    }>
+      (proposal.state == 4 || proposal.state == 7
+        ? "bg-green" : (proposal.state == 2 || proposal.state == 3 || proposal.state == 6
+          ? "bg-red" : "bg-purple"))
+    }>
         {name}
     </div>
   )
